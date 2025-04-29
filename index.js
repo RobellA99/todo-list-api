4d4efc99a768d972af80dfaadf4ddf3b55da0162
Robell <robellasfaw@gmail.com>
2025-04-29 10:59:43 +0100

    Add front end environment variable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONT_END_URL }));
 
 app.get("/", (_req, res) => {
   res.send("App is running...");
